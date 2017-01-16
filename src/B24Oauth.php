b03f98d814aaefb92820e7f3243da11929c698ae
Aleksei Butov <bootoffav@gmail.com>
2017-01-17 01:24:38 +0800

    updated

--- src/B24Oauth.php
+++ src/B24Oauth.php
@@ -4,10 +4,19 @@ namespace bootoffav\laravelBitrix24Oauth;
 
 class B24Oauth
 {
-    public function authorize($request = null, $refresh_token = null)
+    public function handle($request, \Closure $next)
+    {
+        if (! session()->has('b24_credentials') or time() > session()->get('b24_credentials')->expires_at) {
+            return $this->authorize();
+        }
+        
+        return $next($request);
+    }
+
+   private function authorize()
     {
         if (! session()->has('b24_credentials')) {
-            return redirect(config('app.b24_hostname').'/oauth/authorize/?client_id='.config('app.b24_client_id'));
+            return redirect(env('B24_HOSTNAME').'/oauth/authorize/?client_id='.env('B24_CLIENT_ID'));
         }
         if (time() > session('b24_credentials')->expires_at) {
             $cred = $this->getCredentials(null, session('b24_credentials')->refresh_token);
@@ -16,16 +25,17 @@ class B24Oauth
         }
     }
 
-    public function getCredentials($code = null, $refresh_token = null) : string
+    static public function getCredentials($code = null, $refresh_token = null) : string
     {
         if ($code) {
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_URL,
                 'https://oauth.bitrix.info/oauth/token/?grant_type=authorization_code' .
-                '&client_id=' . config('app.b24_client_id') .
-                '&client_secret=' . config('app.b24_client_secret') .
+                '&client_id=' . env('B24_CLIENT_ID') .
+                '&client_secret=' . env('B24_CLIENT_SECRET') .
                 '&code=' . $code);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
             return curl_exec($ch);
         }
 
@@ -33,15 +43,16 @@ class B24Oauth
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_URL,
                 'https://oauth.bitrix.info/oauth/token/?grant_type=refresh_token' .
-                '&client_id=' . config('app.b24_client_id') .
-                '&client_secret=' . config('app.b24_client_secret') .
+                '&client_id=' . env('B24_CLIENT_ID') .
+                '&client_secret=' . env('B24_CLIENT_SECRET') .
                 '&refresh_token=' . $refresh_token);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
             return curl_exec($ch);
         }
     }
 
-    public function setCredentials(string $credentials)
+    private function setCredentials(string $credentials)
     {
         $credentials = json_decode($credentials);
         $credentials->expires_at = time() + $credentials->expires_in;
